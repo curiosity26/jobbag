0ebacf27ef9f564862a77b6767a1e8ade4b74e32
Alex Boyce <curiosity26@gmail.com>
2014-02-15 13:26:36 -0500

    Cleaning up default Rules config. Reformatted code for jobbag_roles. Fixed inconsistencies and cleaned up some obscurities.

--- jobbag_roles/jobbag_roles.controller.php
+++ jobbag_roles/jobbag_roles.controller.php
@@ -10,20 +10,19 @@ class JobRoleControllerExportable extends EntityAPIControllerExportable {
 }
 
 class JobRoleUIController extends EntityDefaultUIController {
-	
-    public function hook_menu() {
-            $items = parent::hook_menu();
-	    $items[$this->path]['type']['title'] = t('Job Roles');
-            $items[$this->path]['type'] = MENU_LOCAL_ACTION;
-            $items[$this->path]['tab_parent'] = 'admin/jobs';
-            $items[$this->path]['tab_root'] = 'admin/jobs';
-            $items[$this->path]['context'] = MENU_CONTEXT_PAGE | MENU_CONTEXT_INLINE;
-            
-            return $items;
-    }
-    
-    public function overviewForm($form, &$form_state) {
-	drupal_set_title('Job Roles');
-	return parent::overviewForm($form, $form_state);
-    }
+  public function hook_menu() {
+    $items = parent::hook_menu();
+    $items[$this->path]['type']['title'] = t('Job Roles');
+    $items[$this->path]['type'] = MENU_LOCAL_ACTION;
+    $items[$this->path]['tab_parent'] = 'admin/jobs';
+    $items[$this->path]['tab_root'] = 'admin/jobs';
+    $items[$this->path]['context'] = MENU_CONTEXT_PAGE | MENU_CONTEXT_INLINE;
+
+    return $items;
+  }
+
+  public function overviewForm($form, &$form_state) {
+    drupal_set_title('Job Roles');
+    return parent::overviewForm($form, $form_state);
+  }
 }
